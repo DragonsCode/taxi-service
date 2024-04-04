8f635e4071ccd5d32921c378c2df1a4038440023
Drago <84837099+DragonsCode@users.noreply.github.com>
2024-04-04 19:17:53 +0500

    Add files via upload

--- taxist/settings.py
+++ taxist/settings.py
@@ -149,3 +149,5 @@ AUTH_USER_MODEL = "taxi.CustomUser"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
